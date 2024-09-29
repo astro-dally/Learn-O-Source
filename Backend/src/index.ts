400822c5b98f9085a9fb9c27458ee3ddf5feb2d6
astro_dally <animemedia64@gmail.com>
2024-09-29 06:18:37 +0530

    updated the file structure

---
+++ Backend/src/index.ts
@@ -0,0 +1,10 @@
+import express,{Express,Request,Response} from "express";
+
+const app:Express = express()
+
+app.get('/', (req:Request, res:Response) => {
+    res.send('working');
+});
+
+
+app.listen(3000,()=>{console.log("app working")})
