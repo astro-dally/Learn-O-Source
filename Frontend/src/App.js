b26a7735cdb2d85c224d920d100548f7c10d5797
astro_dally <animemedia64@gmail.com>
2024-11-07 05:01:47 +0530

    updated

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/landingpage/LandingPage';
 import MainPage from './components/mainpage/MainPage';
-import Login from './components/auth/login.js';
+import Login from './components/auth/Login.js';
 import Signup from './components/auth/SignUp.js';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
